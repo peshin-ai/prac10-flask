8a50db7317369b97c7544cebe198455bbf9a5f38
hoangtuanvp2011 <t00aienl@gmail.com>
2021-01-21 14:54:27 +0700

    complete flask +wiki

--- app.py
+++ app.py
@@ -1,12 +1,44 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for, session
+import wikipedia
 
 app = Flask(__name__)
 
 
 @app.route('/greet')
-@app.route('/greet/Tuan')
-def greet(name="Ung Ta Hoang Tuan"):
-    return "Hello {}".format(name)
+def greet():
+    return "<h1>Hello World :)</h1>"
+
+@app.route('/search', methods=['POST', 'GET'])
+def search():
+    if request.method == 'POST':
+        session['search_term'] = request.form['search']
+        return redirect(url_for('results'))
+    return render_template("index.html")
+
+
+@app.route('/results')
+def results():
+    search_term = session['search_term']
+    page = get_page(search_term)
+    return render_template("results.html", page=page)
+
+
+def get_page(search_term):
+    try:
+        page = wikipedia.page(search_term)
+    except wikipedia.exceptions.PageError:
+        # no such page, return a random one
+        page = wikipedia.page(wikipedia.random())
+    except wikipedia.exceptions.DisambiguationError:
+        # this is a disambiguation page, get the first real page (close enough)
+        page_titles = wikipedia.search(search_term)
+        # sometimes the next page has the same name (different caps), so don't try the same again
+        if page_titles[1].lower() == page_titles[0].lower():
+            title = page_titles[2]
+        else:
+            title = page_titles[1]
+        page = get_page(wikipedia.page(title))
+    return page
 
 
 if __name__ == '__main__':
